d7ef714caab1163c4a67b91112c08a3cf66f62cd
Shah <shah.minul.amin@gmail.com>
2025-08-29 12:37:35 +0100

    Populate quotes data using db migration

---
+++ src/main/resources/db/migration/V2__populate_quotes.sql
@@ -0,0 +1,40 @@
+
+INSERT INTO public."quotes"
+(id, "quote", person, embedding)
+VALUES(1, 'Be yourself; everyone else is already taken.', 'Oscar Wilde', NULL);
+
+INSERT INTO public."quotes"
+(id, "quote", person, embedding)
+VALUES(3, 'Two things are infinite: the universe and human stupidity; and I amm not sure about the universe', 'Albert Einstein', NULL);
+
+INSERT INTO public."quotes"
+(id, "quote", person, embedding)
+VALUES(4, 'A room without books is like a body without a soul.', 'Marcus Tullius Cicero', NULL);
+
+INSERT INTO public."quotes"
+(id, "quote", person, embedding)
+VALUES(7, 'Always forgive your enemies; nothing annoys them so much.', 'Oscar Wilde', NULL);
+
+INSERT INTO public."quotes"
+(id, "quote", person, embedding)
+VALUES(8, 'Live as if you were to die tomorrow. Learn as if you were to live forever.', 'Mahatma Gandhi ', NULL);
+
+INSERT INTO public."quotes"
+(id, "quote", person, embedding)
+VALUES(2, 'So many books, so little time.', 'Frank Zappa', NULL);
+
+INSERT INTO public."quotes"
+(id, "quote", person, embedding)
+VALUES(5, 'Be who you are and say what you feel, because those who mind do not matter, and those who matter do not mind.', 'Bernard M. Baruch ', NULL);
+
+INSERT INTO public."quotes"
+(id, "quote", person, embedding)
+VALUES(6, 'If you tell the truth, you do not have to remember anything.', 'Mark Twain', NULL);
+
+INSERT INTO public."quotes"
+(id, "quote", person, embedding)
+VALUES(9, 'Being deeply loved by someone gives you strength, while loving someone deeply gives you courage.', 'Lao Tzu', NULL);
+
+INSERT INTO public."quotes"
+(id, "quote", person, embedding)
+VALUES(10, 'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.', 'Martin Luther King', NULL);
